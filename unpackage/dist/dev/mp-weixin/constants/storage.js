b49a6492e54a0fe684f6be0943aea682cc19001f
Alen Zhang <1464253423@qq.com>
2025-12-10 14:21:10 +0800

    chore: remove unpackage from git tracking

--- unpackage/dist/dev/mp-weixin/constants/storage.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-const STORAGE_KEYS = {
-  selectedCity: "app:selected-city"
-};
-exports.STORAGE_KEYS = STORAGE_KEYS;
-//# sourceMappingURL=../../.sourcemap/mp-weixin/constants/storage.js.map
