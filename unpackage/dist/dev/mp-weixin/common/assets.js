94fb4de8916a36cacefd97e01aba34ed83e279e0
Alen Zhang <1464253423@qq.com>
2025-12-10 14:21:10 +0800

    chore: remove unpackage from git tracking

--- unpackage/dist/dev/mp-weixin/common/assets.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-const locationIcon = "/static/icons/dingwei.png";
-const dingwei_right = "/static/icons/dingwei_right.png";
-exports.dingwei_right = dingwei_right;
-exports.locationIcon = locationIcon;
-//# sourceMappingURL=../../.sourcemap/mp-weixin/common/assets.js.map
